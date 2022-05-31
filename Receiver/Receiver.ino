e5c411e34413fe0ca14c4f62188396bb28d9b840
Yoshitake Kitamura <kitamura@epea.co.jp>
2022-05-31 18:47:17 +0900

    :wq最低限の通信ができる段階
    ほぼExampleのもの。
    動作しなかったのでチャンネルだけ指定

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,61 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+#define CHANNEL 3
+// 動作確認用LEDのGPIOPIN なんか光らせたり
+#define SIGNAL_PIN 2
+
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    ESP.restart();
+  }
+}
+
+void configDeviceAP() {
+  const char *SSID = "OBAKE_1";
+  bool result = WiFi.softAP(SSID, "OBAKE_NO_PW", CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+  }
+}
+
+void setup() {
+  /*
+   * 速度9600なのはTeraTermで見るときの初期値がそうなっているので合わせているだけ。
+   * 特に速度の影響出たわけではないので必要なら上げても多分OK。
+   */
+  Serial.begin(9600);
+  Serial.println("");
+  Serial.println("OBAKE_1 Reseiver Start");
+
+  pinMode(SIGNAL_PIN, OUTPUT);
+
+  WiFi.mode(WIFI_AP);
+  configDeviceAP();
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+  InitESPNow();
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+// TODO モーター実装。とりあえずPWM強度と秒数受けて動くぐらい？
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+  Serial.println("");
+}
+
+boolean led_flg = true;
+void loop() {
+  digitalWrite(SIGNAL_PIN, led_flg = !led_flg );
+  delay(10000);
+}
